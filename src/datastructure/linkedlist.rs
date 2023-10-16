8b69ae8ec4eead13f4976c061ba33b6d644490ee
Travis Mundy <mundyt4@mymacewan.ca>
2023-10-15 19:51:11 -0600

    Basic new node function added

--- src/datastructure/linkedlist.rs
+++ src/datastructure/linkedlist.rs
@@ -1,3 +1,50 @@
-pub fn test() {
-    println!("hello");
+
+use std::rc::{Rc, Weak};
+use std::cell::{RefCell};
+
+/*
+ * Purpose implement a doubly link list
+ * that will help manage tasks
+ * */
+
+
+
+
+/*
+ * id to determine a task 
+ * rank to determine priority
+ * state will need to change to a different struct
+ * */
+struct Task {
+    id: u32,
+    rank: u32,
+    state: i32 // will change to a task struct
 }
+
+// struct for the node in linklist
+struct Node {
+    node: Rc<RefCell<Task>>,
+    next: Option<Rc<RefCell<Node>>>,
+    prev: Option<Weak<RefCell<Node>>>,
+}
+
+impl Node {
+    fn new(task: Task) -> Rc<RefCell<Self>> {
+        
+        Rc::new(RefCell::new(Node {
+            node: Rc::new(RefCell::new(task)),
+            next: None,
+            prev: None,
+        }))
+    }
+}
+
+// the structure of a doubly linked list
+struct LinkList {
+    head: Option<Rc<RefCell<Node>>>,
+    tail: Option<Weak<RefCell<Node>>>,
+}
+
+
+
+
