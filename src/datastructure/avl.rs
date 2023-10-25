4e05a179f6a7f427192565cf0a30cc73ab4fe609
Travis <mundyt4@mymacewan.ca>
2023-10-24 18:05:35 -0600

    insert basics

--- src/datastructure/avl.rs
+++ src/datastructure/avl.rs
@@ -10,6 +10,7 @@ use std::cmp::Ordering;
 #[derive(Debug, Clone)]
 pub struct AvlTree {
     val: Task,
+    height: i32,
     left: Option<Arc<Mutex<AvlTree>>>,
     right: Option<Arc<Mutex<AvlTree>>>,
 }
@@ -18,20 +19,51 @@ impl AvlTree {
     fn new(task: Task) -> Self {
         AvlTree { 
             val: task, 
+            height: 1,
             left: None, 
             right: None, 
         }
         
     }
+    //insert
+    pub fn insert(&mut self, new_val: Task) {
+        match new_val.rank.cmp(&self.val.rank) {
+            //if rank is less then
+            Ordering::Less => {
+                if let Some(left) = &mut self.left {
+                    left.lock().unwrap().insert(new_val);
+                } else {
+                    let new_node = AvlTree::new(new_val);
+                    self.left = Some(Arc::new(Mutex::new(new_node)));
+                }
+            }
+            //if rank is greater
+            Ordering::Greater => {
+                if let Some(right) = &mut self.right {
+                    right.lock().unwrap().insert(new_val);
+                } else {
+                    let new_node = AvlTree::new(new_val);
+                    self.right = Some(Arc::new(Mutex::new(new_node)));
+                }
+            }
+            Ordering::Equal => {
+                //figure out how to deal with equal compare
+                println!("they equal");
+            }
+            //if ranks are equal
+        }
+        //update height 
+        //update balance
+    }
+    //balance
+    //Delete
+    //traverse
+    //update priority
+    //concurrency
+    //look into preemption
+
 }
 
-//insert
-//balance
-//Delete
-//traverse
-//update priority
-//concurrency
-//look into preemption
 pub fn testing() {
     
     let tasks = vec![
